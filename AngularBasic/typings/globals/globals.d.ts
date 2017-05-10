b5af3c662d91d779f67390cfb9206235d4ac96f4
Matt Jeanes <matthew.jeanes@moneybarn.com>
2017-05-10 15:54:43 +0100

    Add support for bundling html/css files

--- AngularBasic/typings/globals/globals.d.ts
+++ AngularBasic/typings/globals/globals.d.ts
@@ -2,3 +2,13 @@
 
 declare var module: any;
 declare var require: any;
+
+declare module "*.css" {
+    let string: string;
+    export default string;
+}
+
+declare module "*.html" {
+    let string: string;
+    export default string;
+}
