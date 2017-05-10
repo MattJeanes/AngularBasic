36b5c266fccaf78a3d799ad2eb1820e3db5cd2c7
Matt Jeanes <matthew.jeanes@moneybarn.com>
2017-05-10 16:39:48 +0100

    Use import for app template

--- AngularBasic/wwwroot/app/app.component.ts
+++ AngularBasic/wwwroot/app/app.component.ts
@@ -1,8 +1,9 @@
 ﻿import { Component } from '@angular/core';
+import template from './app.template.html';
 
 @Component({
     selector: 'my-app',
     moduleId: module.id,
-    templateUrl: 'app.template.html'
+    template: template
 })
 export class AppComponent { }
