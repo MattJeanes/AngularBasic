36186da0c9a2e5ec3a785ce7d4fe56ba1efb29d9
Matt Jeanes <matthew.jeanes@moneybarn.com>
2018-06-11 12:26:03 +0100

    Remove unnecessary comments

--- AngularBasic/Startup.cs
+++ AngularBasic/Startup.cs
@@ -26,10 +26,8 @@ namespace AngularBasic
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
             services.AddHttpsRedirection(options =>
             {
@@ -37,7 +35,6 @@ namespace AngularBasic
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
